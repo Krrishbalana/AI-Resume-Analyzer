071196a54ef66bebfc8097393f417de73a45ff61
Krrishbalana <krrishbalana@gmail.com>
2025-07-30 15:25:12 +0530

    define types of data

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,58 +1,51 @@
-interface Job {
-    title: string;
-    description: string;
-    location: string;
-    requiredSkills: string[];
-  }
-  
-  interface Resume {
-    id: string;
-    companyName?: string;
-    jobTitle?: string;
-    imagePath: string;
-    resumePath: string;
-    feedback: Feedback;
-  }
-  
-  interface Feedback {
-    overallScore: number;
-    ATS: {
+interface Resume {
+  id: string;
+  companyName?: string;
+  jobTitle?: string;
+  imagePath: string;
+  resumePath: string;
+  feedback: Feedback;
+}
+
+interface Feedback {
+  overallScore: number;
+  ATS: {
       score: number;
       tips: {
-        type: "good" | "improve";
-        tip: string;
+          type: "good" | "improve";
+          tip: string;
       }[];
-    };
-    toneAndStyle: {
+  };
+  toneAndStyle: {
       score: number;
       tips: {
-        type: "good" | "improve";
-        tip: string;
-        explanation: string;
+          type: "good" | "improve";
+          tip: string;
+          explanation: string;
       }[];
-    };
-    content: {
+  };
+  content: {
       score: number;
       tips: {
-        type: "good" | "improve";
-        tip: string;
-        explanation: string;
+          type: "good" | "improve";
+          tip: string;
+          explanation: string;
       }[];
-    };
-    structure: {
+  };
+  structure: {
       score: number;
       tips: {
-        type: "good" | "improve";
-        tip: string;
-        explanation: string;
+          type: "good" | "improve";
+          tip: string;
+          explanation: string;
       }[];
-    };
-    skills: {
+  };
+  skills: {
       score: number;
       tips: {
-        type: "good" | "improve";
-        tip: string;
-        explanation: string;
+          type: "good" | "improve";
+          tip: string;
+          explanation: string;
       }[];
-    };
-  }
+  };
+}
